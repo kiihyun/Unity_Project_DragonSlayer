797df011334be43bd1ccf8a975eebd2760b2aa6f
mak2u-park <scorio1234@gmail.com>
2025-06-14 16:10:33 +0900

    feat : Inventory

--- Assets/02_Scripts/UI/Inventory.cs
+++ Assets/02_Scripts/UI/Inventory.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public List<ItemData> InventoryItems { get { return inventoryItems; }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private List<ItemData> inventoryItems =  new List<ItemData>();
+    
+    
 }
