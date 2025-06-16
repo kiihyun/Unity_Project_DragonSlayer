f1354f8abb42ff304b6ea5a4c1604573e4304f0b
Sungju Kim <tjdwn0885@naver.com>
2025-06-16 11:15:00 +0900

    refactor: 화살 피격시 사라지게 수정
    화살에 맞으면 사라지게 수정했습니다

--- Assets/02_Scripts/Enemy/EnemyProjectile.cs
+++ Assets/02_Scripts/Enemy/EnemyProjectile.cs
@@ -33,6 +33,7 @@ public class EnemyProjectile : MonoBehaviour
         if(collision.TryGetComponent<IDamageable>(out var player))
         {
             player.TakeDamage(10);
+            this.gameObject.SetActive(false);
         }
 
     }
