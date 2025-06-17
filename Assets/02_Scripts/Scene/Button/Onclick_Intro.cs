ff1b02f273ec3a768c19d642c8d2b2a2afc831c1
Sungju Kim <tjdwn0885@naver.com>
2025-06-17 15:52:37 +0900

    feat: 씬 추가, 몬스터 피격 애니메이션 수정
    피격시 더 잘보이게 바꿨습니다

---
+++ Assets/02_Scripts/Scene/Button/Onclick_Intro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onclick_Intro : MonoBehaviour
+{
+    private Animator _animator;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    public void OnClicked()
+    {
+        _animator.SetTrigger("Click");
+    }
+}
---
+++ Assets/02_Scripts/Scene/SceneMonster_Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMonster_Move : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.velocity += Vector2.left*0.5f;
+    }
+}
