14e5974a7250b8e3a7b550c2f128f9a13d03dc4e
kihyun <gosof@naver.com>
2025-06-18 17:42:21 +0900

    add: DragonDeathSound추가
    -dragonDeathSound추가

--- Assets/02_Scripts/Boss/StateMachine/Boss/BossDieState.cs
+++ Assets/02_Scripts/Boss/StateMachine/Boss/BossDieState.cs
@@ -20,6 +20,7 @@ public class BossDieState : IBossState
         Animator animator = _boss.GetComponent<Animator>();
         if (animator != null)
         {
+            SoundManager.Instance.PlaySFX("DragonDeath");
             animator.SetTrigger("Die");
         }
 
@@ -59,7 +60,7 @@ public class BossDieState : IBossState
     private void GiveRewards()
     {
         Debug.Log("보상 지급: 경험치 + 아이템 드랍 등");
-        
+        //_boss.PlayerTarget.
         // 예: GameManager.Instance.AddExp(300);
         // 예: Instantiate(dropItem, _boss.transform.position, Quaternion.identity);
     }
