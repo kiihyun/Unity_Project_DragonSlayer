aa76d2b09098d2027aed49dd1e85676d2a27d6a3
mak2u-park <scorio1234@gmail.com>
2025-06-18 17:22:14 +0900

    feat : 사운드 크기 조절

--- Assets/02_Scripts/UI/WindowUI/UIOption.cs
+++ Assets/02_Scripts/UI/WindowUI/UIOption.cs
@@ -7,24 +7,12 @@ using UnityEngine.UI;
 public class UIOption : BaseWindow
 {
     [SerializeField] private Button MainMenuButon;
-    [SerializeField] private Slider SouondSlider;
     public override UIType UIType => UIType.UIOption;
 
-    private void Start()
-    {
-        SouondSlider.onValueChanged.AddListener(OnSliderValueChanged);
-    }
-    
     public void OnMainMenuButton()
     {
         SceneManager.LoadScene("StartScene");
     }
-    
-    public void OnSliderValueChanged(float value)
-    {
-        // Log10 변환(-80~0dB) : 0.0001 -> -80, 1 -> 0, 데시벨이 로그 스케일이기 때문
-        // .SetFloat("BGM", Mathf.Log10(value) * 20);
-    }
-    
+
     
 }
