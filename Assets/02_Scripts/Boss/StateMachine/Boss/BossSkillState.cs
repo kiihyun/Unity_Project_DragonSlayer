261f0c7647539b1ad04019730b37b66b3d3d1536
kihyun <gosof@naver.com>
2025-06-18 13:14:47 +0900

    fix: bossSkillState수정
    -bossSkillState수정

--- Assets/02_Scripts/Boss/StateMachine/Boss/BossSkillState.cs
+++ Assets/02_Scripts/Boss/StateMachine/Boss/BossSkillState.cs
@@ -32,14 +32,12 @@ public class BossSkillState : IBossState
         _elapsedTime += Time.deltaTime;
         var skill = skills[_boss.SkillIndex];
 
-        //�ߺ����� ����
         if (!_isSkillCasting && _elapsedTime >= skill.delayBeforeCast)
         {
             CastSkill(skill);
             _isSkillCasting = true;
         }
 
-        // ��ų ���� �� ���� ��ų�� �Ѿ��
         // if (_isSkillCasting && _elapsedTime >= skill.delayBeforeCast + skill.cooldown)
         // {
         //     _elapsedTime = 0f;
@@ -60,37 +58,30 @@ public class BossSkillState : IBossState
     {
         Debug.Log($"[CastSkill] index:{skill} " +
                   $"name:{skill.skillName} type:{skill.skillType}");
-        _boss.CurrentSkillData = skill;   // ���� SpawnSkillEffect ���� ����
-
+        _boss.CurrentSkillData = skill;   
         switch (skill.skillType)
         {
-            /* ������ ����(Leap Smash) �� ���� State ������ */
-            case SkillType.LeapSmash:                              // �� enum�� �߰�
+            case SkillType.LeapSmash:                              
                 _boss.StateMachine.ChangeState(
-                    new BossLeapState(_boss, skill));              // ��
-                return;                                            // ���⼭ �ٷ� ����
+                    new BossLeapState(_boss, skill));              
+                return;                                            
 
-            /* ������ Breath ������ */
             case SkillType.Breath:
                 _boss.Animator.SetTrigger(skill.animationTriggerName);
                 break;
 
-            /* ������ Fire Rain ������ */
             case SkillType.FireRain:
                 _boss.Animator.SetTrigger(skill.animationTriggerName);
                 break;
 
-            /* ������ Flame March ������ */
             case SkillType.FlameMarch:
                 _boss.Animator.SetTrigger(skill.animationTriggerName);
                 break;
 
-            /* ������ Sword Wind ������ */
             case SkillType.SwordWind:
                 _boss.Animator.SetTrigger(skill.animationTriggerName);
                 break;
 
-            /* ������ ���� ó�� ������ */
             default:
                 Debug.LogWarning($"���ǵ��� ���� SkillType : {skill.skillType}");
                 break;
